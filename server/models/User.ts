f5786bf7489bb53c1d6864776543afe48fc8edfb
Mukund Thakur <mukund.th04@gmail.com>
2025-03-22 22:23:41 +0530

    Add files via upload

---
+++ server/models/User.ts
@@ -0,0 +1,68 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  profilePicture?: string;
+  dietPreferences?: string[];
+  favoriteIngredients?: string[];
+  favoriteCuisines?: string[];
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUser>({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  profilePicture: {
+    type: String
+  },
+  dietPreferences: {
+    type: [String],
+    default: []
+  },
+  favoriteIngredients: {
+    type: [String],
+    default: []
+  },
+  favoriteCuisines: {
+    type: [String],
+    default: []
+  }
+}, { 
+  timestamps: true 
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+// Method to compare password
+UserSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser>('User', UserSchema);
+
+export default User;
